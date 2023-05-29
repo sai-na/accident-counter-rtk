b9c0d5918faa1fa6fa0c4424333476ec39aaf190
sai-na <saipkmm100@gmail.com>
2023-05-29 15:06:23 +0530

    createReducer and linked into component

--- src/components/counter.tsx
+++ src/components/counter.tsx
@@ -1,16 +1,28 @@
+import { useReducer, useState } from "react";
+import { byInput, counterReducer, decrement, increment, reset } from "../features/counter-reducer";
+
 const Counter = () => {
+  const [state ,dispatch] = useReducer(counterReducer , {count : 0})
+  const [value , setValue] = useState(0)
+
+  
+  const onSubmit  = (e :React.FormEvent<HTMLFormElement> )=> {
+    e.preventDefault()
+    dispatch(byInput(value))
+
+  }
   return (
     <section className="flex flex-col items-center w-2/3 gap-8 p-8 bg-white border-4 shadow-lg border-primary-500">
       <h1>Days Since the Last Accident</h1>
-      <p className="text-6xl">0</p>
+      <p className="text-6xl">{state.count}</p>
       <div className="flex gap-2">
-        <button>➖ Decrement</button>
-        <button>🔁 Reset</button>
-        <button>➕ Increment</button>
+        <button onClick={()=>dispatch(decrement(1))  }>➖ Decrement</button>
+        <button onClick={()=>dispatch(reset())}>🔁 Reset</button>
+        <button onClick={()=>dispatch(increment(1))}>➕ Increment</button>
       </div>
       <div>
-        <form onSubmit={(e) => e.preventDefault()}>
-          <input type="number" value={0} onChange={() => {}} />
+        <form onSubmit={(e)=>onSubmit(e)}>
+          <input type="number" value={value} onChange={(e) => {setValue(e.target.valueAsNumber)}} />
           <button type="submit">Update Counter</button>
         </form>
       </div>
--- src/features/counter-reducer.ts
+++ src/features/counter-reducer.ts
@@ -1,20 +1,40 @@
-import { createAction } from '@reduxjs/toolkit';
+import { createAction, createReducer } from '@reduxjs/toolkit';
 
 type CounterState = { count: number };
 
 
 
-  const increment = createAction('INCREMENT' , (amount : number) : {payload : number} => {
+ export const increment = createAction('INCREMENT' , (amount : number) : {payload : number} => {
     return {payload : amount}
   } )
 
-  const decrement = createAction ("DECREMENT" , (amount : number) : {payload : number} =>{
+  export const decrement = createAction ("DECREMENT" , (amount : number) : {payload : number} =>{
     return {payload :amount}
   } )
 
-  const reset = createAction ("RESET")
+  export const byInput = createAction ("BYINPUT" , (amount : number) =>{
+    return {payload :amount}
+  })
+
+  export const reset = createAction ("RESET")
 
 type CounterAction =  ReturnType<typeof increment> | ReturnType <typeof decrement> | ReturnType < typeof reset>
+
+
+export const counterReducer = createReducer({count : 0}, (builder) => {
+  builder.addCase(increment,(state,action )=>{
+    state.count += action.payload
+  })
+  builder.addCase(decrement , (state ,action)=>{
+    state.count -= action.payload
+  })
+  builder.addCase(byInput , (state ,action) =>{
+    state.count += action.payload    
+  } )
+  builder.addCase(reset , (state ,action)=>{
+  state.count = 0
+})
+})
 
 export const reducer = (state: CounterState, action: CounterAction) => {
   if (action.type === increment.type) {
