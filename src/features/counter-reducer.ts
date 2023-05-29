1774514fcadad3b8eda0217142212b0a86e7553f
sai-na <saipkmm100@gmail.com>
2023-05-29 13:24:47 +0530

    createAction

--- src/features/counter-reducer.ts
+++ src/features/counter-reducer.ts
@@ -1,20 +1,31 @@
 import { createAction } from '@reduxjs/toolkit';
 
 type CounterState = { count: number };
-type CounterAction =
-  | { type: 'INCREMENT' | 'DECREMENT'; payload: number }
-  | { type: 'RESET' };
+
+
+
+  const increment = createAction('INCREMENT' , (amount : number) : {payload : number} => {
+    return {payload : amount}
+  } )
+
+  const decrement = createAction ("DECREMENT" , (amount : number) : {payload : number} =>{
+    return {payload :amount}
+  } )
+
+  const reset = createAction ("RESET")
+
+type CounterAction =  ReturnType<typeof increment> | ReturnType <typeof decrement> | ReturnType < typeof reset>
 
 export const reducer = (state: CounterState, action: CounterAction) => {
-  if (action.type === 'INCREMENT') {
+  if (action.type === increment.type) {
     return { count: state.count + action.payload };
   }
 
-  if (action.type === 'DECREMENT') {
+  if (action.type === decrement.type) {
     return { count: state.count - action.payload };
   }
 
-  if (action.type === 'RESET') {
+  if (action.type ===reset.type) {
     return { count: 0 };
   }
 
